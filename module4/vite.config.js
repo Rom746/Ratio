77dbc545e22c7cbbb7dc32c1cc656d2ef44b523d
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-30 22:13:48 +0300

    Добавлен баннер

--- module4/vite.config.js
+++ module4/vite.config.js
@@ -1,12 +1,20 @@
-const { resolve } = require('path');
-const { defineConfig } = require('vite');
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 module.exports = defineConfig({
+  root,
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        // nested: resolve(__dirname, 'nested/index.html')
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+        blog: resolve(root, 'blog', 'index.html')
       }
     }
   }
