df3c5f730e56810e137d90a09956764e8a33af5e
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-30 18:36:31 +0300

    Добавлен хедер

--- module4/src/js/main.js
+++ module4/src/js/main.js
@@ -1,6 +1,20 @@
 import '../css/style.scss'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const burgerMenu = () => {
+    const menu = document.querySelector('.menu__list');
+    const button = document.querySelector('.menu__button');
+    const buttonLine = document.querySelector('.menu__button-line');
+    const links = document.querySelectorAll('.menu__list-link');
+
+    const toggleMenu = () => {
+        buttonLine.classList.toggle('menu__button-line--active');
+        menu.classList.toggle('menu__list--active');
+        document.body.classList.toggle('--lock');
+    }
+
+    button.addEventListener('click', toggleMenu);
+    links.forEach(link => link.addEventListener('click', toggleMenu));
+}
+
+burgerMenu();
+
